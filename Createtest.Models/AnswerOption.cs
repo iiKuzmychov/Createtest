1f600640377f10c161e312ecbe886b4cb4d68579
kuzcode <igormakw228@gmail.com>
2022-10-16 17:09:04 +0300

    Add AnswerOption class.

---
+++ Createtest.Models/AnswerOption.cs
@@ -0,0 +1,21 @@
+﻿namespace Createtest.Models;
+
+public class AnswerOption
+{
+    private string _text = string.Empty;
+
+    public Guid Guid { get; }
+    public string Text
+    {
+        get => _text;
+        init => _text = value ?? throw new ArgumentNullException(nameof(value));
+    }
+    public Uri? ImageUrl { get; init; }
+
+    public AnswerOption() : this(Guid.NewGuid()) { }
+
+    public AnswerOption(Guid guid)
+    {
+        Guid = guid;
+    }
+}
