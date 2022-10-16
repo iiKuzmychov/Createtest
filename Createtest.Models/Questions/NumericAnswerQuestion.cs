caec697a6e4294f2bc0f59b49bdcc864a622ef1a
kuzcode <igormakw228@gmail.com>
2022-10-16 17:15:19 +0300

    Add NumericAnswerQuestion class

---
+++ Createtest.Models/Questions/NumericAnswerQuestion.cs
@@ -0,0 +1,6 @@
+﻿namespace Createtest.Models.Questions;
+
+public class NumericAnswerQuestion : Question<double>
+{
+    public NumericAnswerQuestion(Guid guid, double correctAnswer) : base(guid, correctAnswer) { }
+}
