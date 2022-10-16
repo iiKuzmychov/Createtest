8c48fa3176d8ba4960386780c32b90bbe26203ad
kuzcode <igormakw228@gmail.com>
2022-10-16 17:05:51 +0300

    Add IQuestion interface.

---
+++ Createtest.Models/Questions/IQuestion.cs
@@ -0,0 +1,10 @@
+﻿namespace Createtest.Models.Questions;
+
+public interface IQuestion
+{
+    public Guid Guid { get; }
+    public string Text { get; }
+    public Uri? ImageUrl { get; }
+    public int Points { get; }
+    public object CorrectAnswer { get; }
+}
