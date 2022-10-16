c0d24096ed103d2fca61e8e38e6523eeb863acfb
kuzcode <igormakw228@gmail.com>
2022-10-16 17:18:08 +0300

    Add TestVariant class

---
+++ Createtest.Models/TestVariant.cs
@@ -0,0 +1,26 @@
+﻿using Createtest.Models.Questions;
+
+namespace Createtest.Models;
+
+public class TestVariant
+{
+    private string _name = string.Empty;
+    private IList<IQuestion> _questions = Array.Empty<IQuestion>();
+
+    public Guid Guid { get; }
+    public string Name
+    {
+        get => _name;
+        set => _name = value ?? throw new ArgumentNullException(nameof(value));
+    }
+    public IList<IQuestion> Questions
+    {
+        get => _questions;
+        set => _questions = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public TestVariant(Guid guid)
+    {
+        Guid = guid;
+    }
+}
