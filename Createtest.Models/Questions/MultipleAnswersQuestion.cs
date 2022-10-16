50ac51baf0563e13bfc3398190429db4ab9850ac
kuzcode <igormakw228@gmail.com>
2022-10-16 17:06:19 +0300

    Add MultipleAnswersQuestion class.

---
+++ Createtest.Models/Questions/MultipleAnswersQuestion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Createtest.Models.Questions;
+
+public class MultipleAnswersQuestion : Question<ReadOnlyCollection<AnswerOption>>
+{
+    public ReadOnlyCollection<AnswerOption> Options { get; }
+
+    public MultipleAnswersQuestion(Guid guid, IList<AnswerOption> options, IList<AnswerOption> correctAnswer)
+        : base(guid, new ReadOnlyCollection<AnswerOption>(correctAnswer))
+    {
+        ArgumentNullException.ThrowIfNull(options);
+
+        if (options.Count < 2)
+            throw new ArgumentException("The options array must contain at least 2 elements.", nameof(options));
+
+        if (options.Except(correctAnswer).Any())
+            throw new ArgumentException("The options array does not contain the correct answer.", nameof(options));
+
+        Options = new ReadOnlyCollection<AnswerOption>(options);
+    }
+}
