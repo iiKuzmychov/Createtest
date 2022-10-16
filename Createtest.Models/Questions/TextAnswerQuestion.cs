6b67c7d3c26ab0031e502a45153800005f6e79a6
kuzcode <igormakw228@gmail.com>
2022-10-16 17:16:12 +0300

    Add TextAnswerQuestion class

---
+++ Createtest.Models/Questions/TextAnswerQuestion.cs
@@ -0,0 +1,13 @@
+﻿namespace Createtest.Models.Questions;
+
+public class TextAnswerQuestion : Question<string>
+{
+    public TextAnswerQuestion(Guid guid, string correctAnswer) : base(guid, correctAnswer)
+    {
+        if (correctAnswer is null)
+            throw new ArgumentNullException(nameof(correctAnswer));
+
+        if (string.IsNullOrWhiteSpace(correctAnswer))
+            throw new ArgumentException("The correct answer is empty string.", nameof(correctAnswer));
+    }
+}
