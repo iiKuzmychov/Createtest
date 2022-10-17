68fead4d6364145e4eea288d08b96f8b8875e862
kuzcode <igormakw228@gmail.com>
2022-10-17 21:03:58 +0300

    Add OrderedanswerOption class

---
+++ src/Createtest.Models/Questions/OrderedAnswerOption.cs
@@ -0,0 +1,12 @@
+﻿namespace Createtest.Models.Questions;
+
+public class OrderedAnswerOption : AnswerOption
+{
+    private int _order = 1;
+
+    public int Order
+    {
+        get => _order;
+        init => _order = (value >= 1) ? value : throw new ArgumentOutOfRangeException(nameof(value));
+    }
+}
