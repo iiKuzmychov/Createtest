454554891db5a9b2abe1e472ac8e05d1fe387974
kuzcode <igormakw228@gmail.com>
2022-10-17 19:56:04 +0300

    Add SequenceQuestion class

---
+++ src/Createtest.Models/Questions/SequenceQuestion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Createtest.Models.Questions;
+
+public class SequenceQuestion : Question<ReadOnlyCollection<AnswerOption>>
+{
+    public SequenceQuestion(Guid guid, IList<AnswerOption> correctAnswer)
+        : base(guid, new(correctAnswer))
+    {
+        ArgumentNullException.ThrowIfNull(correctAnswer);
+
+        if (correctAnswer.Count < 2)
+            throw new ArgumentException("The correct answer list must contain at least 2 elements.", nameof(correctAnswer));
+    }
+}
