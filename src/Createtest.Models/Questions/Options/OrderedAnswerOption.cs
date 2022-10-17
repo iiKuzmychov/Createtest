36615fbce1329d98253c8dd8e7c70bf64c66cccf
kuzcode <igormakw228@gmail.com>
2022-10-17 21:10:49 +0300

    Fix 68fead4d commit

--- src/Createtest.Models/Questions/Options/OrderedAnswerOption.cs
+++ src/Createtest.Models/Questions/Options/OrderedAnswerOption.cs
@@ -7,6 +7,8 @@ public class OrderedAnswerOption : AnswerOption
     public int Order
     {
         get => _order;
-        init => _order = value >= 1 ? value : throw new ArgumentOutOfRangeException(nameof(value));
+        init => _order = (value >= 1) ? value : throw new ArgumentOutOfRangeException(nameof(value));
     }
+
+    public OrderedAnswerOption(Guid guid) : base(guid) { }
 }
