b1af895c3a12e5971408b0f65029953aa68f4996
kuzcode <igormakw228@gmail.com>
2022-10-20 21:14:31 +0300

    Update SequenceQuestion class

--- src/Createtest.Models/Questions/SequenceQuestion.cs
+++ src/Createtest.Models/Questions/SequenceQuestion.cs
@@ -3,14 +3,19 @@ using System.Collections.ObjectModel;
 
 namespace Createtest.Models.Questions;
 
-public class SequenceQuestion : Question<ReadOnlyCollection<AnswerOption>>
+public class SequenceQuestion : Question<ReadOnlyCollection<OrderedAnswerOption>>
 {
-    public SequenceQuestion(Guid guid, IList<AnswerOption> correctAnswer)
+    public SequenceQuestion(Guid guid, IList<OrderedAnswerOption> correctAnswer)
         : base(guid, new(correctAnswer))
     {
         ArgumentNullException.ThrowIfNull(correctAnswer);
 
         if (correctAnswer.Count < 2)
-            throw new ArgumentException("The correct answer list must contain at least 2 elements.", nameof(correctAnswer));
+            throw new ArgumentException("The correct answer must contain at least 2 options.", nameof(correctAnswer));
+
+        if (correctAnswer.Count != correctAnswer.DistinctBy(answer => answer.Order).Count())
+            throw new ArgumentException("The correct answer contains 2 options of the same order.", nameof(correctAnswer));
+
+        // подумать о Distinct
     }
 }
